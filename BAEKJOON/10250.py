0643e191c8033c56bcccad37ec58f2ca8c4f69ab
HyunjiKim818 <kjhxxxx@gmail.com>
2022-03-06 03:46:04 +0900

    BaekJoon 10250

---
+++ BAEKJOON/10250.py
@@ -0,0 +1,14 @@
+T = int(input())
+
+for i in range (T) :
+    H, W, N = map(int, input().split())
+
+    if  N % H == 0 :
+        floor = H
+        room = N // H
+    
+    else :
+        floor = N % H
+        room = N // H + 1
+
+    print(floor*100 + room)
