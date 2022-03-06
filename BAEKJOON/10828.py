d843e8271d3ff8477ed66b6f9a63918134926fd3
HyunjiKim818 <kjhxxxx@gmail.com>
2022-03-07 01:45:36 +0900

    BaekJoon 10828

---
+++ BAEKJOON/10828.py
@@ -0,0 +1,33 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+stack = []
+
+for _ in range(N) :
+    i = sys.stdin.readline().split()
+
+    if i[0] == 'push' :
+        stack.append(int(i[1]))
+
+    elif i[0] == 'pop' :
+        if not stack :
+            print(-1)
+        else :
+            print(stack[-1])
+            stack.pop()
+    
+    elif i[0] == 'size' :
+        print(len(stack))
+
+    elif i[0] == 'empty' :
+        if len(stack) == 0 :
+            print(1)
+        else :
+            print(0)
+    
+    elif i[0] == 'top' :
+        if not stack :
+            print(-1)
+        else :
+            print(stack[-1])
