f642488f11912b0c438f24e42aed8cde46dcb1e9
HyunjiKim818 <kjhxxxx@gmail.com>
2022-03-25 23:13:24 +0900

    BaekJoon 2579

---
+++ BAEKJOON/2579.py
@@ -0,0 +1,18 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+stair = [0]*301
+dp = [0]*301
+
+for i in range(1, T+1) :
+    stair[i] = int(sys.stdin.readline())
+
+dp[1] = stair[1]
+dp[2] = stair[1] + stair[2]
+dp[3] = max(stair[1] + stair[3], stair[2] + stair[3])
+
+for i in range(4, T+1) :
+    dp[i] = max(stair[i] + stair[i - 1] + dp[i - 3], stair[i] + dp[i - 2])
+
+print(dp[T])
